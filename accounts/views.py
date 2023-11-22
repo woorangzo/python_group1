b412000ada1410897db2750dd837a77bbc07aeb1
[hansamk97] <[hansamk1997@gamil.com]>
2023-11-22 13:46:49 +0900

    회원가입/로그인 벨리데이션 체크, 마이페이지 수정

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
+
 from django.views.generic import TemplateView
 
 
@@ -83,3 +84,4 @@ def analyze(request):
 
 def theme(request):
     return render(request, 'accounts/theme.html')
+
