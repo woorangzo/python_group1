aacd1f601adf418e4cdb9451bee8ed14eb6b830a
[hansamk97] <[hansamk1997@gamil.com]>
2023-11-24 17:52:26 +0900

    [ver 0.6] 뉴스페이지 수정, 관련주 비교 수정중

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from . import views
-from .views import join, plot_stock_prices, plot_get_stock_prices, plot_industry_stock_prices
+from .views import join, plot_stock_prices, plot_get_stock_prices
 from django.conf.urls.static import static
 
 
@@ -35,7 +35,7 @@ urlpatterns = [
     path('join/', join, name="join"),
     path('plot_stock_prices/', plot_stock_prices, name='plot_stock_prices'),
     path('plot_get_stock_prices/', plot_get_stock_prices, name='plot_get_stock_prices'),
-    path('plot_industry_stock_prices/', plot_industry_stock_prices, name='plot_industry_stock_prices'),
+    # path('plot_industry_stock_prices/', plot_industry_stock_prices, name='plot_industry_stock_prices'),
     path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html", redirect_authenticated_user=False),
          name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
