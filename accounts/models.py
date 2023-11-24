0f27265137200db93f8b32327abb2bc894636d8e
yumyumsom <woosw13@gmail.com>
2023-11-24 17:50:17 +0900

    [Ver 1.0] 중간 합병

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class Member(models.Model):
-    member_id = models.CharField(max_length=100,primary_key=True)
+    member_id = models.CharField(max_length=100, primary_key=True)
     member_pw = models.CharField(max_length=100)
-    member_repw = models.CharField(max_length=100,default='')
+    member_repw = models.CharField(max_length=100, default='')
     phone = models.CharField(max_length=15, unique=True)
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(null=True)
@@ -13,25 +13,6 @@ class Member(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-class StockData(models.Model):
-    stock_cd = models.CharField(max_length=20)
-    stock_dt = models.DateField()
-    stock_volume = models.BigIntegerField()
-    stock_rate = models.FloatField()
-    open_price = models.DecimalField(max_digits=10, decimal_places=2)
-    close_price = models.DecimalField(max_digits=10, decimal_places=2)
-    high_price = models.DecimalField(max_digits=10, decimal_places=2)
-    low_price = models.DecimalField(max_digits=10, decimal_places=2)
-    plot_image = models.ImageField(upload_to='pic/', blank=True, null=True)
-    prediction_image = models.ImageField(upload_to='pic/', blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.stock_cd} - {self.stock_dt}"
-    # def get_plot_image_url(self):
-    #     if self.plot_image:
-    #         return self.plot_image.url
-    #     return None
 
 class StockData(models.Model):
     stock_cd = models.CharField(max_length=20)
--- accounts/urls.py
+++ accounts/urls.py
@@ -8,8 +8,6 @@ from .views import join
 app_name = 'accounts'
 
 urlpatterns = [
-    # Django에서 지원하는 views 라이브러리를 활용 (로그인, 로그아웃, 비밀번호 찾기 & 초기화 등)
-
     path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html", redirect_authenticated_user=False),
          name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
@@ -24,6 +22,7 @@ urlpatterns = [
     path('analyze/', views.analyze),
     path('theme/', views.theme),
     path('calc/', views.calc),
+    path('blank/', views.blank),
 
 
 
