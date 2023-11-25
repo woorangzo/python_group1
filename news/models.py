0f8bc0e7f2800159d9686e8fd0c52b62a6809d53
sehwaniii <ha9948s@naver.com>
2023-11-25 15:40:25 +0900

    [Ver 0.5] news.models 수정

--- news/models.py
+++ news/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Issue(models.Model):
+    issue_id = models.AutoField(primary_key=True)
     issue_date = models.DateField()
     issue_keyword = models.CharField(max_length=100)
     issue_size = models.FloatField()
@@ -15,4 +16,5 @@ class Article(models.Model):
     news_time = models.DateTimeField()
     news_writer = models.CharField(max_length=100)
     news_content = models.TextField()
-
+    news_url = models.URLField()
+    news_type = models.CharField(max_length=20,default="flash")
