ea739e54908714ee92a7927b35739cde99f5ec3a
sehwaniii <ha9948s@naver.com>
2023-11-25 15:38:08 +0900

    [Ver 0.4] 크롤링 기능 수정

--- crawling.py
+++ crawling.py
@@ -1,64 +1,66 @@
+import os
+import json
+import requests
+import time
 from bs4 import BeautifulSoup as bs
-import requests, json, time
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "woorangzo.settings")
+
+import django
+
+django.setup()
+from news.models import Article, Issue
 
 now = time.strftime('%Y-%m-%d')
 base_url = 'https://n.news.naver.com/article/'
 
 
 def crawl_issue():
-    """
-    :return: 당일 이슈 데이터/ 차트 그릴 때 필요함
-    """
-
-    result = []  # 결과 담아 주기 위한 리스트 생성
-
+    result = []
     try:
-        # requests 라이브러리 사용하여 get 방식으로 호출
         resp = requests.get('https://api.thinkpool.com/chart/issue/bubble')
-
-        resp.raise_for_status()  # 응답 확인 > 안되면 error
-
+        resp.raise_for_status()
     except requests.exceptions.RequestException as e:
-        # try ~ except문으로 오류 처리 , 뭐 나올지 몰라서 RequestException으로 함
         print("Error : ", e)
     else:
-        # json 패키지 loads()로 딕셔너리로 만듬 , list 부분만 사용
         issue_list = json.loads(resp.text).get('list')
 
         for issue in issue_list:
-            issue['date'] = now  # 당일 날짜 추가
-            del issue['issn'], issue['avgRatio']  # for 문 돌려서 필요 없는 데이터 삭제
-            result.append(issue)  # result 리스트에 담아서 리턴
-
+            issue['date'] = now
+            del issue['issn'], issue['avgRatio']
+            result.append(issue['id'])
+            Issue(issue_date=issue['date'], issue_keyword=issue['id'], issue_size=issue['size'],
+                  issue_groupid=issue['groupid']).save()
     return result
 
 
 # def crawled_new(data):
 
-def crawl_news_url(keywords):
-    """
-    :param keywords: crawl_issue()의 return 값에서 'id'값만 가져 온다.
-    :return: 네이버 뉴스 상세페이지 주소 리스트를 리턴한다.
-    """
-
-    result = []
-    # for issue in issue_list:
-    #     keywords.append(issue['id'])  # id만 추출해서 리스트에 담음
-
+def crawl_issue_url(keywords):
+    result = {}
     for keyword in keywords:
-        # 검색어로 뉴스 리스트 페이지 크롤링
+        print(keyword)
+        url_list = []
         url = f'https://search.naver.com/search.naver?where=news&query={keyword}&sort=1&office_category=3'
         resp = requests.get(url)
+        time.sleep(1)
+        print(resp.status_code)
+        if (resp.status_code == 200) & resp.ok:
+            soup = bs(resp.text, 'lxml')
+            issue_news_list = soup.find_all('div', class_='info_group')
 
-        if (resp.status_code == 200) & resp.ok:  # 연결 확인
-            soup = bs(resp.text, 'lxml')  # beatifulsoup 객체 생성
-            naver_new_list = soup.find_all('div', class_='info_group')  # 파싱
+            for issue_news in issue_news_list:
+                if len(issue_news.select('a')) == 2:
+                    href = issue_news.select('a')[1]['href']
+                    url_list.append(href)
 
-            for naver_new in naver_new_list:  # 리스트에서 for 문으로 url만 출력
-                if len(naver_new.select('a')) == 2:
-                    a = naver_new.select('a')[1]['href']
-                    result.append(a)
+        result[keyword] = url_list
 
+    return result
+
+
+def crawl_main_url():
+    url_list = []
     main_news_url = 'https://m.stock.naver.com/front-api/v1/news/category?category=mainnews&pageSize=50&page=1'
 
     resp_1 = requests.get(main_news_url)
@@ -68,8 +70,14 @@ def crawl_news_url(keywords):
         for main_news in main_news_list:
             url = main_news['officeId'] + "/" + main_news['articleId']
             news_url = base_url + url
-            result.append(news_url)
+            url_list.append(news_url)
 
+    result = {'main': url_list}
+    return result
+
+
+def crawl_flash_url():
+    url_list = []
     flash_news_url = 'https://m.stock.naver.com/front-api/v1/news/category?category=flashnews&pageSize=50&page=1'
 
     resp_2 = requests.get(flash_news_url)
@@ -79,57 +87,65 @@ def crawl_news_url(keywords):
         for flash_news in flash_news_list:
             url = flash_news['officeId'] + "/" + flash_news['articleId']
             news_url = base_url + url
-            result.append(news_url)
+            url_list.append(news_url)
 
+    result = {'flash': url_list}
     return result
 
 
-def crawl_news_detail(url):
-    """
-    :param officeid: 언론사 아이디
-    :param articleid: 기사 아이디
-    :return: DB에 뉴스 데이터 넣어줌
-    """
+def crawl_news_detail(result):
+    for key, val in result.items():
+        news_type = key
+        news_url_list = val
 
-    # 샘플 상세페이지 > 네이버 뉴스 상세페잉지
+        for news_url in news_url_list:
 
-    resp = requests.get(url)  # 요청
+            resp = requests.get(news_url)  # 요청
 
-    if (resp.status_code == 200) & resp.ok:
-        soup = bs(resp.text, 'lxml')
+            if (resp.status_code == 200) & resp.ok:
+                soup = bs(resp.text, 'lxml')
 
-        ct = soup.find('div', id='ct')  # content 부분
-        news_office = ct.select_one('img.media_end_head_top_logo_img').attrs['title']  # office 언론사 이름
-        news_title = ct.find('h2', id='title_area').get_text()  # news_title 뉴스 제목
-        news_time = ct.find('span', class_='_ARTICLE_DATE_TIME')['data-date-time']  # 뉴스 입력 시간
-        try:
-            news_writer = ct.find('span', class_='byline_s').get_text()  # 작성자
-        except:
-            news_writer = ''  # 작성자
+                ct = soup.find('div', id='ct')  # content 부분
+                office = ct.select_one('img.media_end_head_top_logo_img').attrs['title']  # office 언론사 이름
+                title = ct.find('h2', id='title_area').get_text()  # news_title 뉴스 제목
+                time = ct.find('span', class_='_ARTICLE_DATE_TIME')['data-date-time']  # 뉴스 입력 시간
+                try:
+                    writer = ct.find('span', class_='byline_s').get_text()  # 작성자
+                except:
+                    writer = ''  # 작성자
 
-        news_article = ct.find('article', id='dic_area')  # 기사 내용 article.text
+                article = ct.find('article', id='dic_area')  # 기사 내용 article.text
 
-        if bool(news_article.find('b')):
-            news_article.find('b').decompose()
-            # print(article)
-        if bool(news_article.find('span', class_='end_photo_org')):
-            news_article.find('span', class_='end_photo_org').decompose()
+                if bool(article.find('b')):
+                    article.find('b').decompose()
+                    # print(article)
+                if bool(article.find('span', class_='end_photo_org')):
+                    article.find('span', class_='end_photo_org').decompose()
 
-        if bool(news_article.find('strong')):
-            news_article.find('strong').decompose()
+                if bool(article.find('strong')):
+                    article.find('strong').decompose()
 
-        news_content = news_article.get_text().strip()
+                content = article.get_text().strip()
 
-        return news_office, news_title, news_time, news_writer, news_content  # 수정 예정
+                Article(news_office=office, news_title=title, news_time=time, news_writer=writer, news_content=content,
+                        news_url=news_url, news_type=news_type).save()
+
+    return print("다 했다!")
+
+
+# def add_crawled_news():
 
 
 if __name__ == '__main__':
-    start = time.time() # 테스트용 수정 예정
-    # issue = crawl_issue()
-    # print(issue)
-    # url_list = crawl_news_url(issue)
-    # print(url_list)
-    # for url in url_list:
-    #     detail = crawl_news_detail(url)
-    #     print(detail)
+    start = time.time()  # 테스트용 수정 예정
+    # pk나 not null 조건에서는 vaildation check 필요함.
+    # PK 수정
+    a = crawl_issue()
+    b = crawl_issue_url(a)
+    c = crawl_main_url()
+    d = crawl_flash_url()
+    crawl_news_detail(b)
+    crawl_news_detail(c)
+    crawl_news_detail(d)
+
     print("time :", time.time() - start)
