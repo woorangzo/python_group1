8833b357f4424394e208e6432b2b11049b54dc66
DESKTOP-4APDNML\ksb <subink2@naver.com>
2023-11-29 15:37:56 +0900

    [Ver 2.0] 회원가입 DB연결 완료

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.core.validators import MaxValueValidator
-from accounts.models import Member
+import mysql.connector
 
 class JoinForm(forms.Form):
     member_id = forms.CharField(max_length=100)
@@ -10,21 +9,25 @@ class JoinForm(forms.Form):
     phone = forms.CharField(max_length=15, required=False)
     username = forms.CharField(max_length=20)
     email = forms.EmailField(required=False)
-    jumin = forms.CharField(max_length=100)
+    regisNum = forms.CharField(max_length=100)
 
-    def clean_name(self):
+    def clean_member_id(self):
         member_id = self.cleaned_data['member_id']
-        if Member.objects.filter(member_id=member_id).exists():
+
+        # MySQL connection setup
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="woorangzo",
+            passwd="1234",
+            database="woorangzo"
+        )
+        with mydb.cursor() as mc:
+            sql = "SELECT COUNT(*) FROM Member WHERE member_id = %s"
+            mc.execute(sql, (member_id,))
+            count = mc.fetchone()[0]
+
+        mydb.close()
+        if count > 0:
             raise ValidationError("이미 사용 중인 아이디입니다.")
+
         return member_id
-
-    def clean(self):
-        cleaned_data = super().clean()
-        member_pw = cleaned_data.get('member_pw')
-        member_repw = cleaned_data.get('member_repw')
-
-        # 비밀번호와 비밀번호 확인이 일치하는지 확인
-        if member_pw and member_repw and member_pw != member_repw:
-            raise forms.ValidationError("비밀번호가 일치하지 않습니다.")
-
-        return cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,16 @@
 import random
 
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import  check_password
 from django.contrib import auth
 from django.urls import reverse
-
-
 from django.views.generic import TemplateView
-
-from accounts.forms import JoinForm
-
-# Create your views here.
+from django.http import HttpResponse
+import mysql.connector
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
 from .forms import JoinForm
-
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from .forms import JoinForm
-from .models import Member
-
+from mysql.connector import Error
 
 def join(request):
     if request.method == 'POST':
@@ -28,33 +18,90 @@ def join(request):
         if form.is_valid():
             member_data = form.cleaned_data
             if member_data['member_pw'] == member_data['member_repw']:
-                Member.objects.create(
-                    member_id=member_data['member_id'],
-                    member_pw=member_data['member_pw'],
-                    member_repw=member_data['member_repw'],
-                    username=member_data['username'],
-                    phone=member_data['phone'],
-                    email=member_data['email'],
-                    jumin=member_data['jumin']
-                )
-                return redirect('accounts:login')
-            else:
-                form.add_error('member_repw', '비밀번호가 일치하지 않습니다')
+                try:
+                    masked_password = '*' * len(member_data['member_pw'])
+                    masked_regisNum = '*' * len(member_data['regisNum'])
+
+                    # MySQL connection setup
+                    mydb = mysql.connector.connect(
+                        host="localhost",
+                        user="woorangzo",
+                        passwd="1234",
+                        database="woorangzo"
+                    )
+
+                    # Cursor creation
+                    with mydb.cursor() as mc:
+
+
+                        sql = """
+                            INSERT INTO Member (
+                                member_id, member_pw, phone, username, email, regisNum,
+                                created_at, updated_at
+                            ) VALUES (%s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+                        """
+
+                        mc.execute(sql, (
+                            member_data['member_id'],
+                            masked_password,
+                            member_data['phone'],
+                            member_data['username'],
+                            member_data['email'],
+                            masked_regisNum
+
+                        ))
+
+                    mydb.commit()
+                    print("Form data:", member_data)
+                    return redirect('accounts:login')
+
+                except Error as e:
+                    print("Database Error:", e)
+
+                finally:
+                    # Close the connection
+                    mydb.close()
+
     else:
         form = JoinForm()
 
     return render(request, 'accounts/join.html', {'form': form})
 
 
-def login_view(request):
+def login(request):
     if request.method == "POST":
-        username = request.POST['username']
+        member_id = request.POST['member_id']
         password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None:
-            return redirect('singlepage:index')
-        else:
-            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect'})
+
+        try:
+            # MySQL connection setup
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user="woorangzo",
+                passwd="1234",
+                database="woorangzo"
+            )
+
+            # Cursor creation
+            with mydb.cursor() as mc:
+                sql = """
+                    SELECT * FROM Member
+                    WHERE member_id = %s
+                """
+                mc.execute(sql, (member_id,))
+                user_data = mc.fetchone()
+
+                if user_data and check_password(password, user_data['member_pw']):
+                    # Authentication successful
+                    return redirect('singlepage:index')
+                else:
+                    return render(request, 'accounts/login.html', {'error': 'Username or password is incorrect'})
+
+        except Error as e:
+            print("Database Error:", e)
+        finally:
+            # Close the connection
+            mydb.close()
     else:
         return render(request, 'accounts/login.html')
 
