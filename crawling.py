b737850c33059be387a7be8ab5b7301ad21d0652
sehwaniii <ha9948s@naver.com>
2023-12-04 14:11:14 +0900

    [Ver 1.4] 크롤링 오류 수정 / 중복 값 제거 수정

--- crawling.py
+++ crawling.py
@@ -57,11 +57,17 @@ def crawl_issue(keywords):
     :param keywords: crawl_issue() 에서 리턴 되는 keyword 리스트
     :return: news_id pk 리스트 형식으로 리턴
     """
+    find_all = "select news_id from news"
+    try:
+        mc.execute(find_all)
+    except mysql.connector.Error as err:
+        print('main find_all Error : ', err)
+    prev_data = mc.fetchall()
     result = []
     for keyword in keywords:
         data = []
         news_num = 1
-        while news_num < 30:
+        while news_num < 20:
 
             url = f'https://search.naver.com/search.naver?where=news&query={keyword}&sort=1&office_category=3&start={news_num}'
             resp = requests.get(url)
@@ -80,18 +86,20 @@ def crawl_issue(keywords):
                         url_parameter = url.split('/')
                         news_id = url_parameter[5] + url_parameter[6]
                         result.append(news_id)  # pk
+                        pick = issue_news.find('i', 'spnew ico_pick')
+
+                        if pick in issue_news.find('a', class_='info press'):
+                            pick.decompose()
+                        issue_news_office = issue_news.find('a', class_='info press').get_text()  # 언론사
+                        issue_news_title = issue_news.find('a', class_='news_tit')['title']  # 뉴스 제목
+                        issue_news_content = issue_news.find('a', class_='api_txt_lines dsc_txt_wrap').get_text()[
+                                             0:80]  # 뉴스 내용
+                        if (news_id,) not in prev_data:
+                            data.append((news_id, issue_news_office, issue_news_title, issue_news_content, keyword))
                     except IndexError:
                         pass
 
-                    pick = issue_news.find('i', 'spnew ico_pick')
 
-                    if pick in issue_news.find('a', class_='info press'):
-                        pick.decompose()
-                    issue_news_office = issue_news.find('a', class_='info press').get_text()  # 언론사
-                    issue_news_title = issue_news.find('a', class_='news_tit')['title']  # 뉴스 제목
-                    issue_news_content = issue_news.find('a', class_='api_txt_lines dsc_txt_wrap').get_text()[
-                                         0:80]  # 뉴스 내용
-                    data.append((news_id, issue_news_office, issue_news_title, issue_news_content, keyword))
 
             news_num += 10
 
@@ -116,7 +124,11 @@ def update_issue():
     except mysql.connector.Error as err:
         print("issue update Error:", err)
     keywords = mc.fetchall()
-    result = crawl_issue(keywords)
+    data = []
+    for keyword in keywords:
+        data.append(keyword[0])
+    print(data)
+    result = crawl_issue(data)
     return crawl_news_detail(result)
 
 
@@ -125,6 +137,12 @@ def crawl_main():
     주요 뉴스 크롤링 메소드 / news_id, 언론사, 제목, 내용 일부분, 뉴스 타입 저장
     :return: pk news_id 리스트 형식으로 리턴
     """
+    find_all = "select news_id from news"
+    try:
+        mc.execute(find_all)
+    except mysql.connector.Error as err:
+        print('main find_all Error : ', err)
+    prev_data = mc.fetchall()
     data = []
     result = []
     main_news_url = 'https://m.stock.naver.com/front-api/v1/news/category?category=mainnews&pageSize=50&page=1'
@@ -138,16 +156,16 @@ def crawl_main():
             body = main_news['body'][0:80] + '...'
             title = main_news['title']
             office_nm = main_news['officeName']
-            data.append((news_id, office_nm, title, body, 'main'))
-            result.append(news_id)
+            if (news_id,) not in prev_data:
+                data.append((news_id, office_nm, title, body, 'main'))
+                result.append(news_id)
 
-    sql = "INSERT INTO news (news_id,news_office_nm,news_title,news_content,news_type) VALUES (?,?,?,?,?)"
-    try:
-        mc.executemany(sql, data)
-    except mysql.connector.Error as err:
-        print("main Error:", err)
-        pass
-    mydb.commit()
+        sql = "INSERT INTO news (news_id,news_office_nm,news_title,news_content,news_type) VALUES (?,?,?,?,?)"
+        try:
+            mc.executemany(sql, data)
+        except mysql.connector.Error as err:
+            print("main Error:", err)
+        mydb.commit()
 
     return result
 
@@ -157,6 +175,12 @@ def crawl_flash():
     실시간 뉴스 크롤링 메소드 / news_id, 언론사, 제목, 내용 일부분, 뉴스 타입 저장
     :return: pk news_id 리스트 형식으로 리턴
     """
+    find_all = "select news_id from news"
+    try:
+        mc.execute(find_all)
+    except mysql.connector.Error as err:
+        print('main find_all Error : ', err)
+    prev_data = mc.fetchall()
     data = []
     result = []
     main_news_url = 'https://m.stock.naver.com/front-api/v1/news/category?category=flashnews&pageSize=50&page=1'
@@ -170,15 +194,16 @@ def crawl_flash():
             body = main_news['body'][0:80] + '...'
             title = main_news['title']
             office_nm = main_news['officeName']
-            data.append((news_id, office_nm, title, body, 'flash'))
-            result.append(news_id)
-    sql = "INSERT INTO news (news_id,news_office_nm,news_title,news_content,news_type) VALUES (?,?,?,?,?)"
-    try:
-        mc.executemany(sql, data)
-    except mysql.connector.Error as err:
-        print("flash Error:", err)
-        pass
-    mydb.commit()
+            if  (news_id,) not in prev_data:
+                data.append((news_id, office_nm, title, body, 'flash'))
+                result.append(news_id)
+        sql = "INSERT INTO news (news_id,news_office_nm,news_title,news_content,news_type) VALUES (?,?,?,?,?)"
+        try:
+            mc.executemany(sql, data)
+        except mysql.connector.Error as err:
+            print("flash Error:", err)
+            pass
+        mydb.commit()
 
     return result
 
@@ -195,20 +220,23 @@ def crawl_news_detail(result):
         oid = pk[0:3]
         aid = pk[3:]
         resp = requests.get(base_url + oid + '/' + aid)  # 요청
-
+        # print(base_url + oid + '/' + aid)
         if (resp.status_code == 200) & resp.ok:
             soup = bs(resp.text, 'lxml')
-            ct = soup.find('div', id='ct')
-            content = ct.find('div', id='newsct_article')
-            writer = ct.find('div', class_='byline')
-            content_tag = content.__str__().replace('data-src', 'src')
-            writer_tag = writer.__str__()
-            tag = content_tag + writer_tag
-            tag = tag.translate({ord(i): None for i in '\n'})
-            date = ct.find('span', class_='_ARTICLE_DATE_TIME')['data-date-time']
-            if bool(ct.find('span', class_='_ARTICLE_MODIFY_DATE_TIME')):
-                date = ct.find('span', class_='_ARTICLE_MODIFY_DATE_TIME')['data-modify-date-time']  # 뉴스 입력 시간
-            data.append((date, tag, pk))
+            try:
+                ct = soup.find('div', id='ct')
+                content = ct.find('div', id='newsct_article')
+                writer = ct.find('div', class_='byline')
+                content_tag = content.__str__().replace('data-src', 'src')
+                writer_tag = writer.__str__()
+                tag = content_tag + writer_tag
+                tag = tag.translate({ord(i): None for i in '\n'})
+                date = ct.find('span', class_='_ARTICLE_DATE_TIME')['data-date-time']
+                if bool(ct.find('span', class_='_ARTICLE_MODIFY_DATE_TIME')):
+                    date = ct.find('span', class_='_ARTICLE_MODIFY_DATE_TIME')['data-modify-date-time']  # 뉴스 입력 시간
+                data.append((date, tag, pk))
+            except AttributeError:
+                pass
 
     sql = "UPDATE news SET news_date = %s, news_tag = %s WHERE news_id = %s"
     try:
@@ -217,7 +245,7 @@ def crawl_news_detail(result):
 
     except mysql.connector.Error as err:
         print("detail Error:", err)
-        # pass
+        pass
 
     return print("다 했다!")
 
@@ -233,8 +261,12 @@ def update_main_news():
     except mysql.connector.Error as err:
         print("Error:", err)
     data = mc.fetchall()
-    result = crawl_main()
-    new_main = set(result) - set(data)
+    results = crawl_main()
+    new_main = []
+    for result in results:
+        if (result,) not in data:
+            new_main.append(result)
+
     return crawl_news_detail(new_main)
 
 
@@ -249,22 +281,26 @@ def update_flash_news():
     except mysql.connector.Error as err:
         print("Error:", err)
     data = mc.fetchall()
-    result = crawl_flash()
-    new_main = (set(result) - set(data))
-    return crawl_news_detail(new_main)
+    results = crawl_flash()
+    new_flash = []
+    for result in results:
+        if (result,) not in data:
+            new_flash.append(result)
+
+    return crawl_news_detail(new_flash)
 
 
 if __name__ == '__main__':
     start = time.time()
-    keywords = crawl_issue_keywords()
-    issue_pk = crawl_issue(keywords)
-    main_pk = crawl_main()
-    flash_pk = crawl_flash()
-    crawl_news_detail(issue_pk)
-    crawl_news_detail(main_pk)
-    crawl_news_detail(flash_pk)
+    # keywords = crawl_issue_keywords()
+    # issue_pk = crawl_issue(keywords)
+    # main_pk = crawl_main()
+    # flash_pk = crawl_flash()
+    # crawl_news_detail(issue_pk)
+    # crawl_news_detail(main_pk)
+    # crawl_news_detail(flash_pk)
 
     update_issue()
-    update_main_news()
-    update_flash_news()
+    # update_main_news()
+    # update_flash_news()
     print("time :", time.time() - start)
